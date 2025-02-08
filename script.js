031e0c964adadfdac8afbb08030a90ed3a8008d6
Shanin_daga-as <161916848+Shanindagaas@users.noreply.github.com>
2025-02-08 12:33:58 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// scripts.js
+
+function showHobbies() {
+    alert('My hobbies include reading, traveling, and playing video games.');
+}
+
+// Add an event listener to trigger the showHobbies function
+document.getElementById('hobbiesButton').addEventListener('click', showHobbies);
