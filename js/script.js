bfe9481bada42625867096f8133f1837da53a147
Shanindagaas <its.shaaa09@gmail.com>
2025-02-22 14:28:43 +0800

    BSIT_42A2_Daga-as_stephanie shanin_A2
    BSIT_42A2_Daga-as_stephanie shanin_A2

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// This function will display an alert when the button is clicked
+function showHobbies() {
+    alert('My hobbies include reading, designing, walking, and talking to people!');
+}
+
+
